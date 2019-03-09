a3f025c90b77de36ba99f746fef4f3f02e4ed7f7
Your Name <you@example.com>
2019-03-08 19:04:51 -0600

    added navbar functionality

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Authentication } from '../service/authentication.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  isUserLoggedIn : boolean = false;
 
-  constructor() { }
+  constructor(
+    private authentication:Authentication
+  ) { }
 
   ngOnInit() {
+    this.isUserLoggedIn = this.authentication.isUserLoggedIn();
   }
 
 }
